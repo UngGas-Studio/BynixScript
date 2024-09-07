d83af270031ca758df684b5949efec56e11b176a
UngGasStudio <155298405+UngGasStudio@users.noreply.github.com>
2024-09-07 14:18:20 +0800

    Create script.js

---
+++ docs/script.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
