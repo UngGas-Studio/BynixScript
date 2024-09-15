8cdcd5d960efe9d169d7050e22d867ca468c8de1
UngGasStudio <155298405+UngGasStudio@users.noreply.github.com>
2024-09-15 16:56:43 +0800

    Create help.js

---
+++ bin/help.js
@@ -0,0 +1,10 @@
+console.log("<=====[ BynixScript ]=====>");
+console.log(" ");
+console.log("1. bsr : To run a file with the extension .bs");
+console.log("------");
+console.log("2. bst : To create a compiled file from a file with the extension .bs");
+console.log("------");
+console.log("3. bsp : To print the translation results from a file with the extension .bs, without running the file.");
+console.log(" ");
+console.log("<=========================>");
+console.log("https://github.com/UngGasStudio/BynixScript");
